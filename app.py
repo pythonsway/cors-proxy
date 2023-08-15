ba0954f11c41fac3f2f8b2d3493e192350803ca8
Pythonsway <adam@pythonsway.it>
2023-08-15 14:15:26 +0200

    relative urls handling

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
+from urllib.parse import parse_qs, urlencode, urljoin, urlparse
+
 import requests
-from flask import Flask, request
+from bs4 import BeautifulSoup
+from flask import Flask, Response, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 
-def addTag(html):
-    result = html.replace('href="/', f'href="{request.base_url.rsplit("/", 1)[0]}/')
+def add_tag(html, url):
+    soup = BeautifulSoup(html, 'html.parser')
+    urls = {'href': ['a', 'link'],
+            'src': ['img', 'script']}
+    for attr, tagList in urls.items():
+        for tags in tagList:
+            for tag in soup.findAll(tags):
+                if tag.has_attr(attr):
+                    if not tag[attr].startswith('http'):
+                        # scheme://netloc/path;parameters?query#fragment
+                        url_parsed = urlparse(url)
+                        query_parsed = parse_qs(url_parsed.query)
+                        query_parsed['url'][0] = urljoin(query_parsed['url'][0], tag[attr])
+                        new_query = urlencode(query_parsed, doseq=True)
+                        new_url = url_parsed._replace(query=new_query).geturl()
+                        tag[attr] = new_url
+    result = str(soup)
     return result
 
 
@@ -16,19 +34,32 @@ def hello_world():
     return 'pong'
 
 
-@app.route('/<path:url>', defaults={'bot': None})
-@app.route('/<bot>/<path:url>')
-def proxy(bot, url):
-    if bot == 'g':
-        headers = {'User-Agent': 'Googlebot'}
+@app.route('/proxy')
+def proxy():
+    if url_query := request.args.get('url'):
+        if request.args.get('bot') == 'g':
+            headers = {'User-Agent': 'Googlebot'}
+        else:
+            headers = {'User-Agent': f'{request.user_agent}'}
+        try:
+            r = requests.get(url_query, headers=headers)
+            r.raise_for_status()
+            # exclude "hop-by-hop headers" +  x-frame-options
+            excluded_headers = ['content-encoding', 'content-length',
+                                'transfer-encoding', 'connection', 'x-frame-options']
+            headers = [(name, value) for (name, value) in r.raw.headers.items()
+                       if name.lower() not in excluded_headers]
+            # headers['Content-Security-Policy'] = "default-src 'self'"
+            if 'text/html' in r.headers["content-type"]:
+                content = add_tag(r.text, request.url)
+            else:
+                content = r.content
+            response = Response(content, r.status_code, headers)
+            return response
+        except requests.exceptions.HTTPError as e:
+            return e.response.text
     else:
-        headers = None
-    try:
-        r = requests.get(url, headers=headers)
-        r.raise_for_status()
-        return addTag(r.text)
-    except requests.exceptions.HTTPError as e:
-        return e.response.text
+        return 'no url'
 
 
 if __name__ == '__main__':
